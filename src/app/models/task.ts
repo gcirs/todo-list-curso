c6a14d6e76b793eb81e9af7cacfa5a526b302c71
Genís Matutes Pujol <gmatutesp@cirsa.com>
2019-04-01 16:30:08 +0200

    Add some styles

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -2,11 +2,13 @@ import { Type } from "./type";
 
 export class Task {
   message: string;
+  description: string;
   type: Type = Type.Incomplete;
 
-  constructor(message: string, type: Type) {
+  constructor(message: string, type: Type, description?: string) {
     this.message = message;
     this.type = type;
+    this.description = description;
   }
 
   isComplete = (): boolean => this.type === Type.Complete;
